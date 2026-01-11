0c76f0e8d9152729a607e67ad3ed2c515c9bd1f4
Tran Huu Nguyen Son <146717584+transon999123@users.noreply.github.com>
2026-01-11 15:43:08 +0700

    Update skool.sql

--- backend/skool.sql
+++ backend/skool.sql
@@ -17,7 +17,7 @@ SET FOREIGN_KEY_CHECKS = 1;
 CREATE DATABASE EduClassDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE EduClassDB;
 
--- 1. Bảng User (Gộp Student, Admin, Teacher)
+-- 1. Bảng User 
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL, 
@@ -27,7 +27,6 @@ CREATE TABLE Users (
     
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    middle_name VARCHAR(50),
     avatar_url TEXT DEFAULT NULL,
     
     gender ENUM('Male', 'Female', 'Other'),
@@ -35,18 +34,46 @@ CREATE TABLE Users (
     
     role ENUM('Admin', 'Teacher', 'Student') NOT NULL,
     
-    -- Thuộc tính riêng
-    experience_years INT,
-    teacher_start_date DATE,
-    admin_start_date DATE,
-    
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 1.1. Bảng Students (Mở rộng từ Users)
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY,
+    student_code VARCHAR(20) NOT NULL UNIQUE,
+    major VARCHAR(100),
+    enrollment_year YEAR,
+    gpa DECIMAL(3, 2) DEFAULT 0.00,
+    credits_accumulated INT DEFAULT 0,
+    
+    FOREIGN KEY (student_id) REFERENCES Users(user_id) ON DELETE CASCADE
+);
+
+-- 1.2. Bảng Teachers (Mở rộng từ Users)
+CREATE TABLE Teachers (
+    teacher_id INT PRIMARY KEY,
+    teacher_code VARCHAR(20) NOT NULL UNIQUE,
+    department VARCHAR(100),
+    qualification VARCHAR(50),
+    contract_type ENUM('Full-time', 'Part-time', 'Visiting'),
+    
+    FOREIGN KEY (teacher_id) REFERENCES Users(user_id) ON DELETE CASCADE
+);
+
+-- 1.3. Bảng Admins (Mở rộng từ Users)
+CREATE TABLE Admins (
+    admin_id INT PRIMARY KEY,
+    admin_code VARCHAR(20) NOT NULL UNIQUE,
+    resonsibility_area VARCHAR(100),
+    
+    FOREIGN KEY (admin_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
 -- 2. Bảng Class (Lớp học)
 CREATE TABLE Classes (
     class_id INT AUTO_INCREMENT PRIMARY KEY,
     class_name VARCHAR(100) NOT NULL,
+    class_credits INT DEFAULT 1,
     class_description TEXT,
     
     start_time TIME,
@@ -142,12 +169,14 @@ CREATE TABLE ForumPosts (
 CREATE TABLE Notifications (
     notification_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,          -- Người nhận thông báo
+    event_id INT,                  -- Sự kiện liên quan (nếu có)
     message TEXT NOT NULL,         -- Nội dung thông báo
     link VARCHAR(255),             -- Đường dẫn khi click vào (VD: assignments?id=1)
     is_read BOOLEAN DEFAULT FALSE, -- Trạng thái đã đọc
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
+    FOREIGN KEY (event_id) REFERENCES Event(event_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Quiz (
@@ -155,49 +184,75 @@ CREATE TABLE Quiz (
     class_id INT NOT NULL,
     student_id INT NOT NULL,
     teacher_id INT NOT NULL,
+    quiz_description TEXT,
     quiz_status ENUM('Not Started', 'In Progress', 'Completed') DEFAULT 'Not Started',
     quiz_limit_time INT, -- Thời gian làm bài (phút)
     quiz_allowed_attempts INT DEFAULT 1,
     quiz_scale INT DEFAULT 10,
     quiz_password VARCHAR(100), -- Mật khẩu nếu có
-    quiz_results JSON,
     quiz_deadline_date DATETIME,
     quiz_dealine_time DATETIME,
 
+    quiz_created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    quiz_updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
     FOREIGN KEY (class_id) REFERENCES Classes(class_id) ON DELETE CASCADE,
-    FOREIGN KEY (student_id) REFERENCES Users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (teacher_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (student_id) REFERENCES Students(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (teacher_id) REFERENCES Teachers(user_id) ON DELETE CASCADE,
 );
 
 CREATE TABLE Questions (
-    question_id INT AUTO INCREMENT PRIMARY KEY,
+    question_id INT AUTO_INCREMENT PRIMARY KEY,
+    quiz_id INT NOT NULL,
     question_scale INT DEFAULT 1,
+    question_description TEXT,
+    question_attachment_url TEXT,
     question_type ENUM ('Multiple Choice', 'True/False', 'Fill in the Blank') NOT NULL,
-    question_given_answers JSON NOT NULL,
-    question_correct_answers JSON NOT NULL,
+
+    Foreign KEY (quiz_id) REFERENCES Quiz(quiz_id) ON DELETE CASCADE,
+);
+
+CREATE TABLE CHOICES (
+    choice_id INT AUTO_INCREMENT PRIMARY KEY,
+    question_id INT NOT NULL,
+    choice_description TEXT,
+    is_correct BOOLEAN DEFAULT FALSE,
+
+    FOREIGN KEY (question_id) REFERENCES Questions(question_id) ON DELETE CASCADE,
 );
 
 CREATE TABLE Attempt (
+    attempt_id INT AUTO_INCREMENT PRIMARY KEY,
     student_id INT NOT NULL,
     quiz_id INT NOT NULL,
-    attempt_count INT DEAFAULT 1,
+
+    attempt_count INT DEFAULT 1,
     attempt_duration INT,
     attempt_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    attempt_status BOOLEAN DEFAULT FALSE,
+    attempt_status ENUM('In Progress', 'Completed', 'Graded', 'Not attempted') DEFAULT 'Not attempted',
+    total_score DECIMAL(5,2),
+    teacher_comment TEXT,
+    score_breakdown JSON,
 
-    FOREIGN KEY (student_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+
+    FOREIGN KEY (student_id) REFERENCES Students(user_id) ON DELETE CASCADE,
     FOREIGN KEY (quiz_id) REFERENCES Quiz(quiz_id) ON DELETE CASCADE,
 );
 
-CREATE TABLE CreateQuiz (
-    teacher_id INT NOT NULL,
-    quiz_id INT NOT NULL,
-    create_quiz_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    create_quiz_date DATE,
+Create table StudentAnswer (
+    attempt_id INT NOT NULL,
+    question_id INT NOT NULL,
 
-    FOREIGN KEY (teacher_id) REFERENCES Users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (quiz_id) REFERENCES Quiz(quiz_id) ON DELETE CASCADE,   
+    answer_content TEXT,
+    answer_choice_id INT, -- Dùng cho câu trắc nghiệm
+
+
+    PRIMARY KEY (attempt_id, question_id),
+    FOREIGN KEY (answer_choice_id) REFERENCES CHOICES(choice_id) ON DELETE SET NULL,
+    FOREIGN KEY (attempt_id) REFERENCES Attempt(attempt_id) ON DELETE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES Questions(question_id) ON DELETE CASCADE
 );
+
 
 CREATE TABLE Event (
     event_id INT AUTO INCREMENT PRIMARY KEY,
@@ -205,11 +260,16 @@ CREATE TABLE Event (
     event_description TEXT,
     event_time DATETIME NOT NULL,
     event_duration INT, -- Độ dài sự kiện (phút)
+    event_loop_frequency ENUM('None', 'Daily', 'Weekly', 'Monthly', 'Yearly') DEFAULT 'None',
     event_loop_count INT DEFAULT 0, -- Số lần lặp lại
-    event_created_by INT NOT NULL,
+    event_created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    event_updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
     user_id INT NOT NULL,
     notification_id INT NOT NULL,
 
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (notification_id) REFERENCES Notifications(notification_id) ON DELETE CASCADE,
-)
+);
+
+
